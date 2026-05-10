844ac86866acc689ee26dfcb23e5d363c2fa668d
thomasthaddeus <thaddeus.r.thomas@gmail.com>
2026-05-10 17:47:01 -0500

    fix frontend

--- client/src/components/SearchBar.js
+++
@@ -1,9 +0,0 @@
-// src/components/SearchBar.js
-/** @jsxImportSource @emotion/react */
-import { searchBarStyle } from "./componentStyles";
-
-const SearchBar = () => (
-  <input type="text" placeholder="Search..." css={searchBarStyle} />
-);
-
-export default SearchBar;
